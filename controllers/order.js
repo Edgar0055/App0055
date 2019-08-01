46739e4fc9c982666c1c3ed9b08f15d7b16fe7ff
Edgar0055 <e.rostomian13@gmail.com>
2019-08-01 18:57:26 +0300

    add_create_func

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,21 @@
+const Order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
+
 module.exports.getAll = function(req, res) {
-    
+
 }
 
-module.exports.create = function(req, res) {
-    
+module.exports.create = async function(req, res) {
+    try {
+        const lastOrder = await Order.findOne({ user: req.user.id }).sort({ date: -1 })
+        const maxOrder = lastOrder ? lastOrder.order : 0
+
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: maxOrder + 1
+        })
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
