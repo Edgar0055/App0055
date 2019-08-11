1f17d1cc74ffaa3691dc7abf0e43fc18e0e3666c
Edgar0055 <e.rostomian13@gmail.com>
2019-08-11 11:32:15 +0300

    use environment variable

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
+const $process = require('process');
+const mongoURI = $process.env.MONGODB || 'mongodb+srv://Edgar:edgar2000.r@edgardb-cotcg.mongodb.net/test?retryWrites=true&w=majority';
+
 module.exports = {
-    mongoURI: 'mongodb+srv://Edgar:edgar2000.r@edgardb-cotcg.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: mongoURI,
     jwt: 'dev-jwt'
 }
