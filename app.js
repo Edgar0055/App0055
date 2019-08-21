72d65bfa6fc8750efaf6633b47e4a1095d505719
Edgar0055 <e.rostomian13@gmail.com>
2019-08-21 16:14:34 +0300

    fix bugs

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
 
-if(procces.env.NODE_ENV === 'production') {
+if(proccess.env.NODE_ENV === 'production') {
     app.use(express.static('client/dist/client'));
 
     app.get('*', (req, res) => {
