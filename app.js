38d76a370133c62a74ff1a15c78ed530c853b087
Edgar0055 <e.rostomian13@gmail.com>
2019-08-21 14:20:23 +0300

    fixes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const path = require('path')
 
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
@@ -31,5 +32,17 @@ app.use('/api/analytics', analyticsRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
+
+if(procces.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist/client'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
+        )
+    })
+}
 
 module.exports = app
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-const $process = require('process');
-const mongoURI = $process.env.MONGODB || 'mongodb+srv://Edgar:edgar2000.r@edgardb-cotcg.mongodb.net/test?retryWrites=true&w=majority';
-
-module.exports = {
-    mongoURI: mongoURI,
-    jwt: 'dev-jwt'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
